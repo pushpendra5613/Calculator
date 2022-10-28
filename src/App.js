738b8407fbe19e1d506173da522d6e37afd1c0d5
Pushpendra amoliya <pushpendraamoliya16@gmail.com>
2022-10-28 23:22:37 +0530

    Task 1 : Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['+', '-', '*', '/', '.'];
+
+  const updateCalc = value => {
+    if (ops.includes(value) && calc === '' ||
+      ops.includes(value) && ops.includes(calc.slice(-1))) {
+      return;
+    }
+    if (value === '-' && calc[0] === '0') {
+      setCalc(value);
+      return;
+    }
+    else if ((value === '+' || value === '/' || value === '*') && calc[0] === '0') return;
+    else if (calc[0] === '0') {
+      setCalc(value);
+    }
+    else setCalc(calc + value);
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  }
+  const CreateDig = () => {
+    const dig = [];
+    for (let i = 1; i < 10; i++) {
+      dig.push(
+        <button onClick={() => updateCalc(i.toString())} key={i}>{i}</button>
+      )
+    }
+    dig.push(<button onClick={() => updateCalc('0')}>{0}</button>)
+    return dig;
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+    const value = '0';
+    setCalc(value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='Calculator'>
+        <div className='ResultDisplayScreen'>
+          {calc || "0"}
+        </div>
+        <div className='Operators'>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+          <button onClick={() => updateCalc('*')}>*</button>
+          <button onClick={() => updateCalc('/')}>/</button>
+          <button onClick={deleteLast}>DEL</button>
+        </div>
+        <div className="Digits">
+          {CreateDig()}
+          <button onClick={() => updateCalc('.')}>.</button>
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
     </div>
   );
 }
